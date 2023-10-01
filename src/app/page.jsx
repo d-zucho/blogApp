066a638176031384de33bf783cd6f2c3f0ad4a8a
D_Zucho <danny.nico.dev@outlook.com>
2023-10-01 04:33:48 -0700

    start featured section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Featured from '@/components/featured/Featured'
 import CategoryList from '@/components/categoryList/CategoryList'
 import CardList from '@/components/cardList/CardList'
 import Pagination from '@/components/pagination/Pagination'
-import Menu from '@/components/featured/Featured'
+import Menu from '@/components/menu/Menu'
 
 export default function Home() {
   return (
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,31 @@
+import Image from 'next/image'
 import styles from './featured.module.css'
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.featured_title}>
+        <span className={styles.greeting_name}>Hey! I'm Marik.</span> Checkout
+        some of my rants and creative ideas
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.post__imgContainer}>
+          <Image className={styles.post__image} src='/p1.jpeg' alt='' fill />
+        </div>
+        <div className={styles.post__textContainer}>
+          <h1 className={styles.post__title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.post__desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatibus, quos, quia, quas voluptate repellat dolorum
+            voluptatum, voluptates nemo doloribus quibusdam. Quisquam voluptas
+            voluptate repellat dolorum voluptatum, voluptates nemo doloribus
+            quibusdam.
+          </p>
+          <button className={styles.post__button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Featured
